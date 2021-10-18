f7e35ac1954dcfd0176eecc90198dfa4b6aa6819
Luís Cobucci <lcobucci@gmail.com>
2021-10-19 01:40:18 +0200

    Upgrade lcobucci/coding-standard to v8.0

--- src/BuilderExtension.php
+++ src/BuilderExtension.php
@@ -49,7 +49,7 @@ final class BuilderExtension implements Extension
     {
         $container->setDefinition(
             $config['name'],
-            $this->createContainerDefinition($config['packages'], $config['container_builder'])
+            $this->createContainerDefinition($config['packages'], $config['container_builder']),
         );
     }
 
--- src/ContainerCannotBeBuilt.php
+++ src/ContainerCannotBeBuilt.php
@@ -12,14 +12,14 @@ final class ContainerCannotBeBuilt extends RuntimeException
     public static function nonReadableFile(string $filename): self
     {
         return new self(
-            sprintf('The file "%s" is not readable, is this the correct path?', $filename)
+            sprintf('The file "%s" is not readable, is this the correct path?', $filename),
         );
     }
 
     public static function nonExistingPackage(string $package): self
     {
         return new self(
-            sprintf('The package class "%s" could not be loaded, is it correct?', $package)
+            sprintf('The package class "%s" could not be loaded, is it correct?', $package),
         );
     }
 }
